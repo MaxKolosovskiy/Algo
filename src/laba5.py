84a261969859432b08e626cc4b9bf0c676e81345
MaxKolosovskiy <cs.go.kot666@gmail.com>
2024-04-12 13:05:25 +0300

    add
    add

---
+++ src/laba5.py
@@ -0,0 +1,54 @@
+def bfs(graph, visited, start):
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        visited[vertex] = True
+
+        for neighbor in graph[vertex]:
+            if not visited[neighbor]:
+                queue.append(neighbor)
+
+def find_root_vertices(graph):
+    n = len(graph)
+    visited = [False] * n
+
+    in_degree = [0] * n
+    for vertex in range(n):
+        for neighbor in graph[vertex]:
+            in_degree[neighbor] += 1
+
+    root_vertices = []
+    for vertex in range(n):
+        if in_degree[vertex] == 0:
+            root_vertices.append(vertex)
+
+    for vertex in root_vertices:
+        bfs(graph, visited, vertex)
+
+    if all(visited):
+        return root_vertices
+    else:
+        return -1
+
+def read_graph_from_file(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        n = int(lines[0])
+        graph = [[] for _ in range(n)]
+        for line in lines[1:]:
+            u, v = map(int, line.strip().split())
+            graph[u].append(v)
+        return graph
+
+def write_result_to_file(result, file_name):
+    with open(file_name, 'w') as file:
+        if isinstance(result, list):
+            file.write(" ".join(map(str, result)))
+        else:
+            file.write(str(result))
+
+if __name__ == "__main__":
+    graph = read_graph_from_file("input.txt")
+    root_vertices = find_root_vertices(graph)
+    write_result_to_file(root_vertices, "output.txt")
---
+++ test/test_laba3.py
@@ -0,0 +1,35 @@
+import unittest
+from src.laba3 import TreeNode
+from src.laba3 import sum_of_depths
+
+class TestSumOfDepths(unittest.TestCase):
+    def test_sum_of_depths(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(7)
+        self.assertEqual(sum_of_depths(root), 10)
+
+    def test_sum_of_depths_empty_tree(self):
+        self.assertEqual(sum_of_depths(None), 0)
+
+    def test_sum_of_depths_single_tree(self):
+        root = TreeNode(1)
+        self.assertEqual(sum_of_depths(root), 0)
+
+    def test_sum_of_depths_left_left_left_tree(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.left.left = TreeNode(3)
+        root.left.left.left = TreeNode(4)
+        self.assertEqual(sum_of_depths(root), 6)
+
+    def test_sum_of_depths_right_right_right_tree(self):
+        root = TreeNode(1)
+        root.right = TreeNode(2)
+        root.right.right = TreeNode(3)
+        root.right.right.right = TreeNode(4)
+        self.assertEqual(sum_of_depths(root), 6)
