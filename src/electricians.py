1b89781f67c07bbcead481ac8416fecc3205b75b
MaxKolosovskiy <cs.go.kot666@gmail.com>
2024-05-24 13:28:45 +0300

    changed-laba9
    laba9

--- src/electricians.py
+++ src/electricians.py
@@ -1,35 +1,31 @@
-import itertools
 import math
 
-def calculate_length(heights, w):
-    max_length = 0
-    n = len(heights)
+def calculate_wire_length(distance, previous_height, current_height):
+    return math.sqrt(distance ** 2 + (previous_height - current_height) ** 2)
 
-    #ALL combinations for height
-    #product decart product equivalent to the nested cycle FOR
-    for decart in itertools.product(*[range(1, h + 1) for h in heights]):
-        #we calculate the length of the wire for the current combination of the heights of the supports
-        length = 0
-        for i in range(1, n):
-            length += math.sqrt((w ** 2) + ((decart[i] - decart[i - 1]) ** 2))
+def compute_maximum_wire_length(distance, heights, output_filename):
+    num_pillars = len(heights)
+    wire_lengths = [[0, 0] for _ in range(num_pillars)]
 
-        #srch max
-        max_length = max(max_length, length)
+    for i in range(1, num_pillars):
+        bottom_to_bottom = wire_lengths[i - 1][0] + calculate_wire_length(distance, 1, 1)
+        top_to_bottom = wire_lengths[i - 1][1] + calculate_wire_length(distance, 1, heights[i - 1])
+        wire_lengths[i][0] = max(bottom_to_bottom, top_to_bottom)
 
-    return round(max_length, 2)
+        bottom_to_top = wire_lengths[i - 1][0] + calculate_wire_length(distance, heights[i], 1)
+        top_to_top = wire_lengths[i - 1][1] + calculate_wire_length(distance, heights[i], heights[i - 1])
+        wire_lengths[i][1] = max(bottom_to_top, top_to_top)
 
-def read_input(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        w = int(lines[0].strip())
-        heights = list(map(int, lines[1].strip().split()))
-    return heights, w
+    max_wire_length = round(max(wire_lengths[num_pillars - 1][0], wire_lengths[num_pillars - 1][1]), 2)
 
-def write_output(file_path, result):
-    with open(file_path, 'w') as file:
-        file.write(str(result) + '\n')
+    with open(output_filename, 'w') as file:
+        file.write(f"{max_wire_length}")
 
-if __name__ == "__main__":
-    heights, w = read_input('../resurses/input_v2.txt')
-    result = calculate_length(heights, w)
-    write_output('../resurses/output_v2.txt', result)
+    return max_wire_length
+
+def load_input(filename):
+    with open(filename, 'r') as file:
+        distance = int(file.readline().strip())
+        heights = list(map(int, file.readline().strip().split()))
+
+    return distance, heights
--- test/test_electricians.py
+++ test/test_electricians.py
@@ -1,24 +1,36 @@
 import unittest
 from src.electricians import *
 
-class TestWireLengthCalculator(unittest.TestCase):
-    def test_calculate_wire_length_example_1(self):
-        w = 2
+class TestWireLengthFunctions(unittest.TestCase):
+
+    def test_v1(self):
+        distance = 2
         heights = [3, 3, 3]
-        expected_length = 5.65
-        self.assertAlmostEqual(calculate_length(heights, w), expected_length, delta=0.01)
+        output_filename = "test_output_electricians.txt"
+        result = compute_maximum_wire_length(distance, heights, output_filename)
+        self.assertEqual(result, 5.66)
 
-    def test_calculate_wire_length_example_2(self):
-        w = 100
+    def test_v2(self):
+        distance = 100
         heights = [1, 1, 1, 1]
-        expected_length = 300.0
-        self.assertAlmostEqual(calculate_length(heights, w), expected_length, delta=0.01)
+        output_filename = "test_output_electricians.txt"
+        result = compute_maximum_wire_length(distance, heights, output_filename)
+        self.assertEqual(result, 300)
 
-    def test_calculate_wire_length_example_3(self):
-        w = 4
+    def test_v3(self):
+        distance = 4
         heights = [100, 2, 100, 2, 100]
-        expected_length = 396.32
-        self.assertAlmostEqual(calculate_length(heights, w), expected_length, delta=0.01)
+        output_filename = "test_output_electricians.txt"
+        result = compute_maximum_wire_length(distance, heights, output_filename)
+        self.assertEqual(result, 396.32)
+
+    def test_v4(self):
+        distance = 4
+        heights = [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 66, 28, 2, 95, 97, 60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 3, 93, 34, 57, 51, 11, 39, 72]
+        output_filename = "test_output_electricians.txt"
+        result = compute_maximum_wire_length(distance, heights, output_filename)
+        self.assertEqual(result, 2738.18)
+
 
 if __name__ == '__main__':
     unittest.main()
